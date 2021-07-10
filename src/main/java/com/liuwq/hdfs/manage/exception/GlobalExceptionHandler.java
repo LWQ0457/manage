9c2a172c922b6f2eeb12c81d8cf3cb0e135ef3cc
xiaoqiang <2621853490@qq.com>
2021-07-10 10:14:38 +0800

    全局异常处理

---
+++ src/main/java/com/liuwq/hdfs/manage/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.liuwq.hdfs.manage.exception;
+
+import com.liuwq.hdfs.manage.pojo.ResultBody;
+import com.liuwq.hdfs.manage.pojo.ResultEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private final static Logger logger= LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    @ExceptionHandler(value =Exception.class)
+    @ResponseBody
+    public ResultBody exceptionHandler(HttpServletRequest req, Exception e){
+        logger.error("未知异常！原因是:",e);
+        return ResultBody.error(ResultEnum.INTERNAL_SERVER_ERROR);
+    }
+}
